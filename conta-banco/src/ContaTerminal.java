8a0b08dd1ea647fd78e4a7f6142a615c7e513ba4
alberto nunes <betinhonunes2024@gmail.com>
2025-07-08 00:55:20 -0300

    feat: Adiciona funcionalidade ContaTerminal e atualiza logica de entrada

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+import java.util.Locale;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        System.out.println("Por favor, digite o número da agência:");
+        String agencia = scanner.nextLine();
+        System.out.println("Por favor, digite o número da sua conta:");
+        int numeroConta = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("Por favor, Digite o nome do cliente:");
+        String nomeCliente = scanner.nextLine();
+        
+        System.out.println("Por favor, digite o saldo:");
+        double saldo = scanner.nextDouble();
+        scanner.nextLine();
+
+        scanner.close();
+
+        System.out.println("\nOlá " + nomeCliente + ", obrigado por criar uma conta em nosso banco,");
+        System.out.println("sua agência é " + agencia + ", conta " + numeroConta + " e seu saldo R$ " + String.format(Locale.US,"%.2f", saldo) + " já está disponivel para saque.");
     }
 }
+
+    
